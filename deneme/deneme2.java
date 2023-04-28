bbf5127c2ebf41c8fa7597f693005cd2409495d1
Ahmet Yagmur <ahmtyagmur03@gmail.com>
2023-04-28 12:20:03 +0200

    güncel

--- deneme/deneme2.java
+++ deneme/deneme2.java
@@ -3,6 +3,7 @@ package deneme;
 public class deneme2 {
     public static void main(String[] args) {
         System.out.println("hello again, world");
+        System.out.println("hello twice");
     }
 
 }
